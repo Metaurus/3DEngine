f30766b62eb7ed7eec657199ed9ab5be52aa7d1b
Metaurus <andreprihodko5@gmail.com>
2023-06-21 22:46:09 +0200

    generating model with lighting

--- OpenGLCopy/main.cpp
+++ OpenGLCopy/main.cpp
@@ -81,7 +81,7 @@ void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
         float beta  = (float)(i-(second_half ? t1.y-t0.y : 0))/segment_height; // be careful: with above conditions no division by zero here 
         Vec2i A =               t0 + (t2-t0)*alpha; 
         Vec2i B = second_half ? t1 + (t2-t1)*beta : t0 + (t1-t0)*beta; 
-        if (A.x>B.x) std::swap(A, B); 
+        if (A.x>B.y) std::swap(A, B); 
         for (int j=A.x; j<=B.x; j++) { 
             image.set(j, t0.y+i, color); // attention, due to int casts t0.y+i != A.y 
         } 
@@ -89,5 +89,35 @@ void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
 }
 
 int main(int argc, char** argv) {
+    if (2==argc) {
+        model = new Model(argv[1]);
+    } else {
+        model = new Model("obj/african_head.obj");
+    }
+
     TGAImage image(width, height, TGAImage::RGB);
+    Vec3f light_dir(0, 0, 1);
+    for (int i=0; i<model->nfaces(); i++) {
+        std::vector<int> face = model->face(i);
+        Vec2i screen_coords[3];
+        Vec3f world_coords[3];
+        for (int j=0; j<3; j++) {
+            Vec3f v = model->vert(face[j]);
+            screen_coords[j] = Vec2i((v.x+1.)*width/2.,
+                                     (v.y+1.)*height/2.);
+            world_coords[j] = v;
+        }
+        Vec3f n = (world_coords[2]-world_coords[0])^(world_coords[1]-world_coords[0]); // cross product for normal
+        n.normalize();
+        float intensity = n*light_dir;
+        if (intensity > 0) {
+            triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, TGAColor(intensity*255, intensity*255, intensity*255, 255));
+        }
+        
+    }
+
+    image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+    image.write_tga_file("output.tga");
+    delete model;
+    return 0;
 }
